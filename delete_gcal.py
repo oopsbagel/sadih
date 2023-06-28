42d7263cd1beeb903b09a978a4cc9ab36edb19bf
oopsbagel <oopsbagel@disroot.org>
2023-06-28 03:37:22 -0700

    feat(delete_gcal): delete events in date range

--- delete_gcal.py
+++ delete_gcal.py
@@ -1,7 +1,17 @@
 import os
+from dateutil.parser import parse as dateutil_parse
 from gcsa.google_calendar import GoogleCalendar
 from gcsa.event import Event
+from zoneinfo import ZoneInfo
+
+PACIFIC = ZoneInfo("America/Los_Angeles")
+
+START = '2023-06-14'
+END = '2023-06-17'
+
+start_dt = dateutil_parse(START).replace(tzinfo=PACIFIC)
+end_dt = dateutil_parse(END).replace(tzinfo=PACIFIC)
 
 sadih = GoogleCalendar(os.environ['SADIH_ID'])
-for e in sadih:
+for e in sadih.get_events(start_dt, end_dt):
     sadih.delete_event(e)
